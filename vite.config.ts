fa6c163cf3509a4df8fbd526dff1e22a00376034
70nanon <kakip2332@gmail.com>
2025-04-15 08:55:30 +0900

    settng deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES 
-    ? "reactailwind"
-    : "./",   
+  base: "/hanafuda_point/",
   plugins: [react()],
 })
